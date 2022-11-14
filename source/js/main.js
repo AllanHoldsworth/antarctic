3afd65379891835632d3956181fedc25be6b0ff9
Robert <leifman.robert@gmail.com>
2022-11-14 16:58:43 +0300

    js fix, editorconfig

--- source/js/main.js
+++ source/js/main.js
@@ -28,16 +28,19 @@ window.addEventListener('DOMContentLoaded', () => {
 
   let navMain = document.querySelector('.header__nav');
   let navToggle = document.querySelector('.header__menu-button');
-  navMain.classList.remove('header__nav--nojs');
-  navToggle.addEventListener('click', function () {
-    if (navMain.classList.contains('header__nav--closed')) {
-      navMain.classList.remove('header__nav--closed');
-      navMain.classList.add('header__nav--opened');
-    } else {
-      navMain.classList.add('header__nav--closed');
-      navMain.classList.remove('header__nav--opened');
-    }
-  });
+
+  if (navToggle && navMain) {
+    navMain.classList.remove('header__nav--nojs');
+    navToggle.addEventListener('click', function () {
+      if (navMain.classList.contains('header__nav--closed')) {
+        navMain.classList.remove('header__nav--closed');
+        navMain.classList.add('header__nav--opened');
+      } else {
+        navMain.classList.add('header__nav--closed');
+        navMain.classList.remove('header__nav--opened');
+      }
+    });
+  }
 
   iosVhFix();
 
